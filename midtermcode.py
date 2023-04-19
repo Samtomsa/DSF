48a7bc109a08097f40201d45ef834c89ef8f4598
Samtomsa <126336472+Samtomsa@users.noreply.github.com>
2023-04-19 13:53:12 +0200

    Update midtermcode.py

--- midtermcode.py
+++ midtermcode.py
@@ -20,7 +20,8 @@ from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-%matplotlib inline
+HERE = Path(_dh[-1])
+DATA = HERE / "data"
 
 def smiles_to_fp(smiles, method="maccs", n_bits=2048):
     """
@@ -51,20 +52,8 @@ def smiles_to_fp(smiles, method="maccs", n_bits=2048):
     else:
         print(f"Warning: Wrong method specified: {method}." " Default will be used instead.")
         return np.array(MACCSkeys.GenMACCSKeys(mol))
-        
-def convert_target_id(chembl_code, input_length=25):
-    """Converts a ChEMBL target id to a numpy array of fixed length that can be used as input the neural network"""
-    
-    # Remove the 'CHEMBL' prefix from the ChEMBL code
-    code_integer = int(chembl_code.replace('CHEMBL', ''))
-    
-    # Convert the integer to a numpy array of fixed length
-    binary_str = '{0:b}'.format(code_integer).zfill(input_length)
-    input_array = np.array([float(bit) for bit in binary_str], dtype=np.float64)
-    
-    return input_array
-    
-    def neural_network_model(hidden1, hidden2):
+  
+  def neural_network_model(hidden1, hidden2):
     """
     Creating a neural network from two hidden layers
     using ReLU as activation function in the two hidden layers
@@ -93,48 +82,32 @@ def convert_target_id(chembl_code, input_length=25):
     model.compile(loss="mean_squared_error", optimizer='adam', metrics=["mse", "mae"])
     return model
     
-# Mount Google Drive to access files
-from google.colab import drive
-drive.mount("/content/drive", force_remount=True)
-
-# Load the dataset
-df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/kinase.csv', index_col=0)
+url = "https://cloud-new.gdb.tools/index.php/s/ZfZM7itQf3rm6Sw/download"
+df = pd.read_csv(url, index_col=0)
 df = df.reset_index(drop=True)
 
 # Keep necessary columns
 chembl_df = df[["smiles", "target_chembl_id","standard_value"]]
 # Convert IC50 to pIC50 (standart_value to the pIC50)
-chembl_df['pIC50'] = -np.log10(df['standard_value'])
+chembl_df['pIC50'] = -np.log10(df['standard_value'] * 1e-9)
 
 chembl_df.replace([np.inf, -np.inf], np.nan, inplace=True)
 chembl_df.dropna(inplace=True)
 
 chembl_df["fingerprints_df"] = chembl_df["smiles"].apply(smiles_to_fp)
 
-#binary representation of the chembl ID
-chembl_df["binary_id"] = chembl_df["target_chembl_id"].apply(convert_target_id)
-
-#combiens information from traget and chemical structure
-chembl_df['combined_input'] = chembl_df.apply(lambda row: np.concatenate([row['fingerprints_df'], row['binary_id']]), axis=1)
-
 # Split the data into training and test set
 x_train, x_test, y_train, y_test = train_test_split(
-    chembl_df["combined_input"], chembl_df[["pIC50"]], test_size=0.3, random_state=42
+    chembl_df["fingerprints_df"], chembl_df[["pIC50"]], test_size=0.3, random_state=42
 )
-
 # Neural network parameters
 batch_sizes = [16, 32, 64]
 nb_epoch = 50
 layer1_size = 64
 layer2_size = 32
 
-model = neural_network_model(layer1_size, layer2_size)
-
 # Save the trained model
-weights_folder = "/content/drive/MyDrive/Colab Notebooks/"
-
-# Save the trained model
-filepath = os.path.join(weights_folder, "best_weights.hdf5")
+filepath = DATA / "best_weights.hdf5"
 checkpoint = ModelCheckpoint(
     str(filepath),
     monitor="loss",
